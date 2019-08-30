d5b5227a771dae133e5919dd92ef487b0d9a5f05
finge <finge@DESKTOP-KLSBBPF>
2019-08-29 20:00:57 -0500

    Initial Commit

---
+++ ShogiServer/ShogiServer.java
@@ -0,0 +1,28 @@
+
+public class ShogiServer 
+{	
+	public ShogiServer(String arg)
+	{
+		//Code goes here
+	}
+	
+	public ShogiServer()
+	{
+		//this will eventually be default case
+		//if there are no command line arguments
+		this("");
+	}
+	
+	public static void main(String[] args) 
+	{
+		if(args.length > 0)
+		{
+			new ShogiServer(args[0]);
+		}
+		else 
+		{
+			new ShogiServer();
+		}
+	}
+
+}
